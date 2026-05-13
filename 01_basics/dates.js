e58cafe8b35a78a29de05e3292452a2991264165
imakasx <akashvishwakarma0618l@gmail.com>
2026-05-13 16:17:56 +0530

    date-practice

---
+++ 01_basics/dates.js
@@ -0,0 +1,21 @@
+let myDate = new Date();
+
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+
+//let newDate = new Date(2025,2,1);
+//console.log(newDate.toString());
+
+// let newDate = new Date(2025,2,1,5,30);
+// console.log(newDate.toLocaleString());
+
+let newDates = new Date()
+console.log(newDates.getDay()+1);
+console.log(newDates.getMonth());
+
+// newDates.toLocaleString('default',{
+//     weekend:"long",
+// })
+// // console.log(newDates);
