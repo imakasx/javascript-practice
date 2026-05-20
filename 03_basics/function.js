f58e6bde15c9f00f83d2a69edafa7727186851e5
imakasx <akashvishwakarma0618@gmail.com>
2026-05-20 22:26:42 +0530

    function-practice

---
+++ 03_basics/function.js
@@ -0,0 +1,38 @@
+function myName(){
+    console.log("A");
+    console.log("K");
+    console.log("A");
+    console.log("S");
+    console.log("H");
+}
+// myName();
+
+function addNumber(num1,num2){
+    console.log(num1+num2);
+}
+
+// addNumber(2,5);
+// addNumber(2,"a");
+// addNumber(2, null);
+// const result = addNumber(5,3);
+// console.log("result :",result)
+
+function addTwoNumber(num1,num2){
+    // let result = num1 + num2
+    // return result
+
+    return num1+num2
+    
+}
+const result = addTwoNumber(3,2)
+// console.log("result :",result)
+
+function loginmessage(username){
+    if(!username){
+        console.log("please enter the name ")
+        return 
+    }
+    return `${username} is logged in`
+}
+// console.log(loginmessage())
+console.log(loginmessage("akash"))
