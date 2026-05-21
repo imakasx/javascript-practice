1e2cba5270e975af2b913b93b110e5f545d49ca4
imakasx <akashvishwakarma0618@gmail.com>
2026-05-21 23:57:19 +0530

    scopes-practice

---
+++ 03_basics/scopes.js
@@ -0,0 +1,15 @@
+let c = 45
+if (true) {
+    let c = 450
+    console.log("value", c)
+}
+
+var a = 30
+if (true) {
+    var a = 300
+    console.log("value", a)
+}
+
+console.log(a);
+// console.log(b);
+console.log(c);
