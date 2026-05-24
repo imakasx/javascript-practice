1c7c248209cf19201306e44efa39c7f8b346125b
imakasx <akashvishwakarma0618@gmail.com>
2026-05-24 19:08:25 +0530

    iife-practice

---
+++ 03_basics/iife.js
@@ -0,0 +1,13 @@
+function data(){
+    console.log(`db conected`)
+}
+// data()
+
+// ()()
+(function tree(){
+    console.log(`db conected one`)
+})();
+
+((wall)=>{
+    console.log(`${wall},db conected two`)
+})("akash")
