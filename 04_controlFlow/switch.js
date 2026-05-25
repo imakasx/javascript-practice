3b2e9abc469b74e3267e70b125ccaa1a3278feaf
imakasx <akashvishwakarma0618@gmail.com>
2026-05-25 16:15:04 +0530

    switch-practice

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,53 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("jan")
+//         break;
+//     case 2:
+//         console.log("feb")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+//     case 4:
+//         console.log("may")
+//         break;
+//     case 5:
+//         console.log("june")
+//         break;
+//     case 6:
+//         console.log("july")
+//         break;
+
+//     default:
+//         console.log("default")
+//         break;
+// }
+
+//string keyword  
+
+const file = "datafile"
+switch (file) {
+    case "googlefile":
+        console.log("Is Google file ")
+        break;
+    case "datafile":
+        console.log("Is Data file ")
+        break;
+    case "myfile":
+        console.log("Is My file ")
+        break;
+
+    default:
+        console.log("Default File")
+        break;
+}
