07bad2956d7396581c123b1549b6129b14b47800
imakasx <akashvishwakarma0618@gmail.com>
2026-05-28 12:09:20 +0530

    do-while-loop-practice

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+let i =0;
+while (i<=5) {
+    //console.log(`score is : ${i}`);
+    i++;
+}
+
+let y = 5;
+while (y<=5){
+    // console.log(`score is : ${y}`);  
+    y++
+}
+
+let score = 0;
+do {
+    // console.log(`value is : ${score}`);
+    score++
+} while (score<10);
+
+let j= 11;
+do {
+    console.log(`value is : ${j}`);
+    j++
+} while (j<=10);
