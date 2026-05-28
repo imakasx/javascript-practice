37d931cfae8d9d829ae717da9e3e32da38233b67
imakasx <akashvishwakarma0618@gmail.com>
2026-05-28 16:10:55 +0530

    for in-practice

---
+++ 05_iterations/four.js
@@ -0,0 +1,16 @@
+const lang ={
+    js : "Javascript",
+    cpp : "C++",
+    py: "Python", 
+    java: "jdk"
+}
+
+for (const key in lang) {
+    // console.log(`${key} means ${lang[key]}`)
+}
+
+const prg =["py","cpp","js","java",]
+for (const key in prg) {
+    // console.log(key)
+    console.log(prg[key])
+}
