c6f1cbd41441571aceda63f074c0230c96ed6ab6
imakasx <akashvishwakarma0618@gmail.com>
2026-06-23 16:31:59 +0530

    promises-using-then-catch

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,9 +1,66 @@
-const promise = new Promise(function(resole, reject){
+const promise = new Promise(function(resolve, reject){
     setTimeout(function(){
         console.log("Async resolve");
-        resole();
+        resolve();
     },1000)
 })
 promise.then(function(){
     console.log("promise consumed");
 })
+
+
+new Promise(function(resolve, reject){
+    setTimeout(()=>{
+        console.log("Async resolve 2");
+        resolve()
+    },1000)
+}).then(()=>{
+    console.log("promise consumed 2")
+})
+
+const promisethree = new Promise(function(resolve, reject){
+    setTimeout(()=>{    
+        resolve({
+            username: 'Chai', 
+            email: "Chai@example.com"
+        });
+    },1000)
+})
+
+promisethree.then((user)=>{
+        console.log(user)
+    })
+
+
+const promisefour = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        let error =true;
+        if(!error){
+            resolve({
+            username: "akash",
+            password: "12345"
+        })
+        }else{
+            reject('error: something went wrong ')
+        }
+        
+    },1000)
+})
+
+promisefour
+.then((user)=>{
+    return user.username
+})
+.then((data)=>{
+    console.log(data)
+})
+.catch((error)=>{
+    console.log(error)
+})
+.finally(()=>{
+    console.log("All Done")
+})
+
+
+
+
