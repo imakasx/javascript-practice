7d6c4a73e47ce3aae03df097dee0725b6d89e490
imakasx <akashvishwakarma0618@gmail.com>
2026-06-27 21:59:56 +0530

    object-learn

--- 10_classes_and_oop/objects.js
+++ 10_classes_and_oop/objects.js
@@ -1,3 +1,5 @@
+const { useReducer } = require("react")
+
 function multiplyObj(num){
     return num*10
 }
@@ -5,3 +7,22 @@ multiplyObj.power = 2
 console.log(multiplyObj(8))
 console.log(multiplyObj.prototype)
 console.log(multiplyObj.power)
+
+
+function createUser(username, score){
+    this.username = username
+    this.score = score
+}
+
+createUser.prototype.increment = function(){
+    this.score++
+}
+createUser.prototype.printMe =function(){
+    console.log(`score is ${this.username}`)
+}
+
+const tea =  new createUser("tea", 25);
+const coffe = new createUser("coffe" , 125);
+
+tea.printMe();
+
