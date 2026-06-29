e18400e427d0e116b644a12f0d52188a91400f10
imakasx <akashvishwakarma0618@gmail.com>
2026-06-29 21:26:59 +0530

    prototype-1

---
+++ 10_classes_and_oop/prototype.js
@@ -0,0 +1,15 @@
+let myName = "Akash   ";
+let myUser = "uiAkash  ";
+
+// console.log(myName.trueLength)  
+
+let myHero = ["spider","thor"]
+
+let heroPower ={
+    thor : "hammer",
+    spider : "sling",
+
+    getspiderPower : function () {
+        console.log(`spider power is ${this.spider}`)
+    }
+}
