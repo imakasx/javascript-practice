6e9ac37af13f151df71590dc1be11bc01ff9250b
imakasx <akashvishwakarma0618@gmail.com>
2026-06-29 22:17:16 +0530

    prototype-learning

--- 10_classes_and_oop/prototype.js
+++ 10_classes_and_oop/prototype.js
@@ -26,7 +26,46 @@ Array.prototype.heyakash = function(){
 // myHero.akash()
 // heroPower.heyakash()// not run bcz power only array
 
-myHero.heyakash()
+// myHero.heyakash()
+
+// Inheritance 
+
+const user = {
+    name : "akash",
+    email : "akash@gmail.com",  
+}
+
+const teacher = {
+    makevideo : true
+}
+
+const teachingSupport = {
+    isAvailable : false
+}
+
+const TAsupport= {
+    makeAssigment : 'js assignment',
+    fullTime: true, 
+    __proto__: teachingSupport
+}
 
 
+teacher.__proto__ = user
 
+// modern syntax
+
+
+Object.setPrototypeOf(teachingSupport , teacher)
+
+let anotherName = "akash vishwakarma"
+
+String.prototype.truelength = function(){
+    console.log(this)
+    console.log(this.trim().length)
+    console.log(`True length is ${this.length}`)
+
+}
+
+anotherName.truelength();
+
+"iceTea".truelength();
