fe1af9a79426de3d6818e7c67397b1137a84335b
imakasx <akashvishwakarma0618@gmail.com>
2026-07-01 21:43:45 +0530

    classes-in-js

--- 10_classes_and_oop/myClasses.js
+++ 10_classes_and_oop/myClasses.js
@@ -1,22 +1,43 @@
     // ES6
 
-class user{
-    constructor(email, password, username){
-        this.username = username;
-        this.email = email;
-        this.password = password;
+// class user{
+//     constructor(email, password, username){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
         
-    }
+//     }
     
-    encryptPassword(){
-        return `${this.password}abc`
-    }
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
 
-    changeUsername(){
-        return`${this.username.toUpperCase()}`
-    }
+//     changeUsername(){
+//         return`${this.username.toUpperCase()}`
+//     }
+// }
+// const good = new user("akash@fb.com","1234", "rahul")
+
+// console.log(good.encryptPassword())
+// console.log(good.changeUsername())
+
+// Behind the scene
+
+function User1(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password;
 }
-const good = new user("akash@fb.com","1234", "rahul")
 
-console.log(good.encryptPassword())
-console.log(good.changeUsername())
+User1.prototype.encryptPassword = function(){
+return `${this.password}abc`
+} 
+
+User1.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const ui = new User1("akash","akash@fb.com", "1234")
+
+console.log(ui.encryptPassword())
+console.log(ui.changeUsername())
