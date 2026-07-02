f259d00161dfbcb07218a9e37d988e0ba770a112
imakasx <akashvishwakarma0618@gmail.com>
2026-07-02 21:07:04 +0530

    inharitance-js

--- 10_classes_and_oop/inharitance.js
+++ 10_classes_and_oop/inharitance.js
@@ -8,5 +8,25 @@ class user {
     }
 }
 
-const use = new user("js")
-use.logMe()
+class teacher extends user {
+    constructor(username, email, password){
+        super(username)
+        this.email = email;
+        this.password = password;
+        // new calling keyword
+    }
+
+    addCourse(){
+        console.log(`a new course added by ${this.username}`)
+    }
+}
+
+const use = new teacher("js", "akash@fb.com","321")
+use.addCourse()
+
+const chai = new user("rahul")
+// chai.addCourse()
+chai.logMe()
+
+// console.log(use === teacher)
+console.log(use instanceof teacher)
