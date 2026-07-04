8dcd93d86d3dd5b776c9d783bec6d31565d8a3d8
imakasx <akashvishwakarma0618@gmail.com>
2026-07-04 20:06:16 +0530

    mathPi-js

---
+++ 10_classes_and_oop/mathPi.js
@@ -0,0 +1,14 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+console.log(descripter)
+
+
+// console.log(Math.PI)
+// Math.PI = 5
+// console.log(Math.PI)
+
+const chai = {
+    name : "ginger-chai",
+    price :250, 
+    isAvailble : true
+}
+console.log(chai)
