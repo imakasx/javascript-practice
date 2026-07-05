e064467aa6624f22e47607e3c25d041652f91802
imakasx <akashvishwakarma0618@gmail.com>
2026-07-05 21:46:07 +0530

    getter_setter-js

--- 10_classes_and_oop/getter_setter.js
+++ 10_classes_and_oop/getter_setter.js
@@ -1,10 +1,36 @@
 class user{
+
     constructor(email, password){
         this.email = email
         this.password = password
     }
+
+// password ke liye getter and setter...
+
+    get password(){
+        // return this._password.toUpperCase()
+        return `321@${this._password}op`
+    }
+
+    set password(value){
+        this._password = value
+    }
+
+// email ke liye getter and setter...
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(str){
+        this._email = str
+    }
+
 }
 
 
-const akash = new user("ak@fb.com", "123")
+const akash = new user("ak@fb.com", "abc")
+
 console.log(akash.password)
+console.log(akash.email)
+
