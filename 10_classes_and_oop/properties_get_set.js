b00432838eca756772082624b028ea09ca0fcae1
imakasx <akashvishwakarma0618@gmail.com>
2026-07-06 18:50:19 +0530

    properties-get-set

--- 10_classes_and_oop/properties_get_set.js
+++ 10_classes_and_oop/properties_get_set.js
@@ -1,16 +1,27 @@
-function user(email, username){
+function user(email, password){
     this._email = email
-    this.username = username 
+    this._password = password
 
     Object.defineProperty(this , "email", {
         get: function(){
-            return this._email.toUpperCase()
+            // return this._email.toUpperCase()
+            return `Here is email is : ${this._email.toUpperCase()}`
         },
         set:function(value){
             this.email = value
         }
     })
+
+    Object.defineProperty(this , "password", {
+        get: function(){
+            return ` password is :  ${this._password}erw`
+        },
+        set:function(value){
+            this.password = value
+        }
+    })
 }
 
-const ui = new user ("ak@gmail.com", "rahul")
+const ui = new user ("ak@gmail.com", "6532")
 console.log(ui.email)
+console.log(ui.password)
